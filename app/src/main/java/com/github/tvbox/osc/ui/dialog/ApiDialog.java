4d9f68176469f4059ca7f03b6d67bfd2c90dbe00
hfr1107 <92629769+hfr1107@users.noreply.github.com>
2022-07-26 06:54:46 +0800

    Update ApiDialog.java

--- app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiDialog.java
+++ app/src/main/java/com/github/tvbox/osc/ui/dialog/ApiDialog.java
@@ -55,7 +55,8 @@ public class ApiDialog extends BaseDialog {
         ivQRCode = findViewById(R.id.ivQRCode);
         tvAddress = findViewById(R.id.tvAddress);
         inputApi = findViewById(R.id.input);
-        inputApi.setText(Hawk.get(HawkConfig.API_URL, ""));
+                //内置网络接口在此处添加
+        inputApi.setText(Hawk.get(HawkConfig.API_URL, "http://gg.gg/hfrcs"));
         findViewById(R.id.inputSubmit).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
